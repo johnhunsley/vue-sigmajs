a52a03d2b531803e774b0bc9a5b3661723537775
johnhunsley <snowball33>
2018-03-10 16:54:40 +0000

    initial framework

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # vue-sigmajs
 A Vue.js wrapper for Sigma.js
+
+### Compatibility
+
+- v1 later
+  - Vue.js 2.x
+  - Sigmas v1.2.1
+
+### Install
+
+  - **npm** install: `npm install vue-sigmajs sigma.js --save`
---
+++ src/examples/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="Sigma">
+      <h1 style="text-align:center;">Basic Example</h1>
+      <basic-example></basic-example>
+    </div>
+  </div>
+</template>
+
+<script>
+import BasicExample from './BasicExample.js'
+
+export default {
+  components: {
+    BasicExample
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+    max-width: 800px;
+    margin: 0 auto;
+}
+h1 {
+    font-family: 'Helvetica', Arial;
+    color: #464646;
+    text-transform: uppercase;
+    border-bottom: 1px solid #f1f1f1;
+    padding-bottom: 15px;
+    font-size: 28px;
+    margin-top: 0;
+}
+.Sigma {
+    padding: 20px;
+    box-shadow: 0px 0px 20px 2px rgba(0, 0, 0, .4);
+    border-radius: 20px;
+    margin: 50px 0;
+}
+</style>
